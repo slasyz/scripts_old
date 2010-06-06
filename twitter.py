8df5d0f04a3b3c0fa777ff2356834b9a631937ba
slasyz <slasyz@gmail.com>
2010-06-06 21:50:36 +0400

    small fix

--- twitter.py
+++ twitter.py
@@ -6,19 +6,22 @@ from urllib2 import HTTPError
 from optparse import OptionParser
 
 class TooLong(Exception): pass
+class AuthError(Exception): pass
 
 def main():
     parser = OptionParser()
     parser.add_option('-u', '--username',
-                      type="str", dest="username",
+                      type="str", dest="username", default = '',
                       help='Twitter account username.')
     parser.add_option('-p', '--password',
-                      type="str", dest="password",
+                      type="str", dest="password", default = '',
                       help='Twitter account password.')
     
     args, text = parser.parse_args()
     text = ' '.join(text)
     
+    if not (args.username and args.password) or len(text) == 0:
+        raise AuthError()
     if len(text) > 140:
         raise TooLong()
     res = upload('http://twitter.com/statuses/update.xml', {'status': text}, auth = '%s:%s' % (args.username, args.password))
@@ -31,5 +34,7 @@ if __name__ == '__main__':
         print(u'Сообщение слишком большое (максимальная длина - 140 символов)')
     except KeyboardInterrupt:
         print(u'Выход по Ctrl+C.')
+    except AuthError:
+        print(u'Требуется имя пользователя, пароль и текст сообщения.')
     except HTTPError:
         print(u'Ошибка!')
