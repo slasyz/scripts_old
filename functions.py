85ef7f8d7f5386e945e8e51676aa14fd43b8f312
slasyz <slasyz@gmail.com>
2010-07-10 14:39:44 +0400

    small fix

--- functions.py
+++ functions.py
@@ -1,31 +1,23 @@
 # coding: utf-8
 
-# === paRSSer ===
-# Разработчик: slasyz ( http://juick.com/sl , http://slasyz.blogspot.com/ )
-# Лицензия: GNU GPL v3 ( http://www.gnu.org/licenses/gpl.html )
+# === SimpLoader ===
+# Developed by: slasyz ( http://juick.com/sl )
+# Home page: http://slasyz.github.com/simploader/
+# License: GNU GPL v3 ( http://www.gnu.org/licenses/gpl.html )
 
-import os, re, urllib2, pycurl
-from datetime import datetime
-
-from urllib import urlencode, unquote_plus
+import os, re, urllib2, pycurl, types, subprocess
+from urllib import urlencode
 from StringIO import StringIO
 
-def notify(title, text):
-    link_regexp = re.compile(r'(http://[A-Za-z0-9_/\.]*)')
-    text = re.sub(link_regexp, r'<a href="\1">\1</a>', text)
-    os.popen("notify-send -t 10000 -i '/usr/share/icons/Tango/32x32/actions/go-top.png' '%s' '%s'" % (title, text))
-
-def upload(url, params = {}, cookies = '', headers = [], auth = ''):
-    if params == {}:
-        return urllib2.urlopen(url).read()
-    else:
-        curl = pycurl.Curl()
-        curl.setopt(pycurl.URL, url)
+def upload(url, params = {}, cookies = '', headers = [], auth = '', part = 2): # 1 - headers, 2 - body
+    curl = pycurl.Curl()
+    curl.setopt(pycurl.URL, url)
+    if params != {}:
         curl.setopt(pycurl.POST, 1)
         data = []
         multipart = False
         for key, value in params.items():
-            if value != '':
+            if (value != '') and (type(value) == types.StringType):
                 if value[0] == '@':
                     data.append((key, (pycurl.FORM_FILE, value[1:])))
                     multipart = True
@@ -37,15 +29,24 @@ def upload(url, params = {}, cookies = '', headers = [], auth = ''):
             curl.setopt(pycurl.HTTPPOST, data)
         else:
             curl.setopt(pycurl.POSTFIELDS, urlencode(data))
-        curl.setopt(pycurl.COOKIE, cookies)
-        curl.setopt(pycurl.HTTPHEADER, headers)
-        curl.setopt(pycurl.USERPWD, auth)
-        text = StringIO()
-        curl.setopt(pycurl.WRITEFUNCTION, text.write)
-        curl.perform()
-        
-        code = curl.getinfo(pycurl.HTTP_CODE)
-        if code == 304: # Если принят результат "Not Modified"
-            return '304'
-        else:
-            return text.getvalue()
+    else:
+        curl.setopt(pycurl.POST, 0)
+    curl.setopt(pycurl.COOKIE, cookies)
+    curl.setopt(pycurl.HTTPHEADER, headers)
+    curl.setopt(pycurl.USERPWD, auth)
+    text = StringIO()
+    heads = StringIO()
+    curl.setopt(pycurl.WRITEFUNCTION, text.write)
+    curl.setopt(pycurl.HEADERFUNCTION, heads.write)
+    curl.perform()
+    
+    code = curl.getinfo(pycurl.HTTP_CODE)
+    if code == 304: # Если принят результат "Not Modified"
+        return '304'
+    else:
+        return (heads.getvalue(), text.getvalue())[part-1]
+
+def notify(title, text):
+    link_regexp = re.compile(r'(http://[A-Za-z0-9_/\.]*)')
+    text = re.sub(link_regexp, r'<a href="\1">\1</a>', text)
+    return subprocess.call(['notify-send', '-t', '10000', '-i', 'go-top', title, text])
