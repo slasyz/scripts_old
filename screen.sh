8cc1651e524f743cb915afc0227362c6068ea27b
Vyacheslav Syrovatsky <slasyz@gmail.com>
2016-12-05 21:21:58 +0300

    Chanded upload method to SSH instead of HTTP request.

--- screen.sh
+++ screen.sh
@@ -4,12 +4,12 @@ FILE=`date +"Screenshot-%Y-%m-%d_%H-%M-%S".png`
 HOST="slasyz.ru"
 DESTINATION="$HOME/Pictures/screenshots"
 DATE=`date "+%Y/%m/%d %H:%M:%S"`
-PASSWORD=`cat $HOME/.config/slasyz_ru/upload_password.txt`
+#PASSWORD=`cat $HOME/.config/slasyz_ru/upload_password.txt`
 
 env PATH=$PATH:/home/sl/Documents/bin/slop /home/sl/Documents/bin/maim/maim -s -b1 $DESTINATION/$FILE;
-#scp "$DESTINATION/$FILE" slasyz.ru:~/www/files.slasyz.ru/screenshots;
-csrf_token=`curl $HOST/upload/ | grep csrf | grep -Po "(?<=value=')\w+(?=')"`
-URL=`curl -b csrftoken=$csrf_token -F csrfmiddlewaretoken=$csrf_token -F fileup=@$DESTINATION/$FILE -F password=$PASSWORD $HOST/upload/upload-ajax/ | grep "success-file" | grep -Po "(?<=href=\").+(?=\")"` ;
+scp "$DESTINATION/$FILE" slasyz.ru:~/www/files.slasyz.ru/screenshots;
+URL="http://files.slasyz.ru/$FILE"
+#URL=`curl -b csrftoken=$csrf_token -F csrfmiddlewaretoken=$csrf_token -F fileup=@$DESTINATION/$FILE -F password=$PASSWORD $HOST/upload/upload-ajax/ | grep "success-file" | grep -Po "(?<=href=\").+(?=\")"` ;
 
 echo $URL;
 echo -n $URL | xclip -i;
